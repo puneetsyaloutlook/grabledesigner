8f760204648b67d0a321919577d68f63f99cd348
puneetsyaloutlook <110325372+puneetsyaloutlook@users.noreply.github.com>
2026-09-02 11:30:52 +1000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Features from './pages/Features';
 import Experience from './pages/Experience';
 import Standards from './pages/Standards';
+import Documentation from './pages/Documentation';
 import Framework from './pages/Framework';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Features />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/standards" element={<Standards />} />
+        <Route path="/documentation" element={<Documentation />} />
         <Route path="/framework" element={<Framework />} />
       </Route>
     </Routes>
